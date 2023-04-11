45b1ebc9a94099b3a0f394ce94afb8589d26d385
Andrew So <asoso221@gmail.com>
2023-04-10 20:17:57 -0700

    Update reboot.py
    Update to use dotenv library for better cross os compatibility and security.

--- src/reboot.py
+++ src/reboot.py
@@ -1,15 +1,17 @@
+from dotenv import dotenv_values
 from mcrcon import MCRcon
-import os
 import time
 
-SERVER_IP = os.environ.get('MC_Server_IP', None)                        # Server IP address
-SEC_KEY = os.environ.get('MCRCON_PSWD', None)                           # Rcon password set in the 'server.properties' file
-PORT = os.environ.get('MCRCON_PORT', 25575)                             # 25575 is the default RCON port for Minecraft
+config = dotenv_values()
+SERVER_IP = config.get("SERVER_IP")                   # Server IP address
+SEC_KEY = config.get("SEC_KEY")                       # Rcon password set in the 'server.properties' file
+PORT = config.get("PORT")                             # 25575 is the default RCON port for Minecraft
+
 
 if SERVER_IP is None or SEC_KEY is None:
     raise Exception("Either Server IP or Rcon Password is set to None. Check your environmental variables or manually change it in the code.")
 
-with MCRcon(SERVER_IP, SEC_KEY, PORT) as mcr:
+with MCRcon(SERVER_IP, SEC_KEY, int(PORT)) as mcr:
     def start_cnt_down():
         seconds = [30,45,55]                                            # 30, 15, 10 seconds left
         for i in range(3):
